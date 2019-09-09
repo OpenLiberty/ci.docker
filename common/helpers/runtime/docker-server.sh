fd8f24532536dca898139b121d4690c11aeddefa
Artur Dzmitryieu <dzmitry@ca.ibm.com>
2019-09-09 13:27:00 -0400

    Use defaults folder for keystore instead of overrides

--- common/helpers/runtime/docker-server.sh
+++ common/helpers/runtime/docker-server.sh
@@ -3,23 +3,27 @@
 set -e
 
 SNIPPETS_SOURCE=/opt/ol/helpers/build/configuration_snippets
-SNIPPETS_TARGET=/config/configDropins/overrides
+SNIPPETS_TARGET_DEFAULTS=/config/configDropins/defaults
+SNIPPETS_TARGET_OVERRIDES=/config/configDropins/overrides
 
-keystorePath="$SNIPPETS_TARGET/keystore.xml"
+keystorePath="$SNIPPETS_TARGET_DEFAULTS/keystore.xml"
 
 if [ "$SSL" = "true" ] || [ "$TLS" = "true" ]
 then
-  if [ "$1" = "server" ] || [ "$1" = "/opt/ol/wlp/bin/server" ]
+  cp $SNIPPETS_SOURCE/tls.xml $SNIPPETS_TARGET_OVERRIDES/tls.xml
+fi
+
+if [ "$SSL" != "false" ] && [ "$TLS" != "false" ]
+then
+  if [ ! -e $keystorePath ]
   then
-    if [ ! -e $keystorePath ]
-    then
-      # Generate the keystore.xml
-      export KEYSTOREPWD=$(openssl rand -base64 32)
-      sed -i.bak "s|REPLACE|$KEYSTOREPWD|g" $SNIPPETS_SOURCE/keystore.xml
-      cp $SNIPPETS_SOURCE/keystore.xml $SNIPPETS_TARGET/keystore.xml
-    fi
+    # Generate the keystore.xml
+    export KEYSTOREPWD=$(openssl rand -base64 32)
+    sed -i.bak "s|REPLACE|$KEYSTOREPWD|g" $SNIPPETS_SOURCE/keystore.xml
+    cp $SNIPPETS_SOURCE/keystore.xml $SNIPPETS_TARGET_DEFAULTS/keystore.xml
   fi
 fi
+
 
 # Pass on to the real server run
 exec "$@"
