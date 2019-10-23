8c54f99b9ba3160384ed0491f28b8a9168ee8b59
Arthur De Magalhaes <ademagalhaes@gmail.com>
2019-10-23 16:40:40 -0400

    Updating tags
    Signed-off-by: Arthur De Magalhaes <ademagalhaes@gmail.com>

--- sync-master.sh
+++ sync-master.sh
@@ -2,6 +2,4 @@
 
 find official -type d -name "helpers" | xargs -n 1 cp -a common/helpers/*
 find official -type d -name "configure.sh" | xargs -n 1 cp -a common/helpers/*
-find official -type f -name "configure.sh" | egrep ibmsfj | xargs -n 1 cp -a common/configure_ibmsfj.sh
-
 find community -type d -name "helpers" | xargs -n 1 cp -a common/helpers/*
