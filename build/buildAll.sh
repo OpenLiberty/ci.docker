7917933eb9832cacca722fecf03ca1ebcc3a20db
Arthur De Magalhaes <ademagalhaes@gmail.com>
2019-11-14 21:17:13 -0500

    Update build
    Signed-off-by: Arthur De Magalhaes <ademagalhaes@gmail.com>

--- build/buildAll.sh
+++ build/buildAll.sh
@@ -4,12 +4,11 @@ repository=$1
 tests=(test-pet-clinic test-stock-quote test-stock-trader)
 
 echo "Starting to process repository $repository"
-cd $repository
 
 # Builds up the build.sh call to build each individual docker image listed in images.txt
 while read -r buildContextDirectory dockerfile repository imageTag imageTag2 imageTag3
 do
-  buildCommand="./build.sh --dir=$buildContextDirectory  --dockerfile=$dockerfile --tag=$repository:$imageTag"
+  buildCommand="./build.sh --dir=$repository/$buildContextDirectory  --dockerfile=$dockerfile --tag=$repository:$imageTag"
   if [ ! -z "$imageTag2" ]
   then
     buildCommand="$buildCommand --tag2=$repository:$imageTag2"
@@ -36,5 +35,5 @@ do
     echo "Failed at image $imageTag ($buildContextDirectory) - exiting"
     exit 1
   fi
-done < "images.txt"
+done < "$repository/images.txt"
 
