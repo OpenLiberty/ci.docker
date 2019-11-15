bf69747fda722be0f30e1b92e735d9e900f99391
Arthur De Magalhaes <ademagalhaes@gmail.com>
2019-11-14 20:58:38 -0500

    Split build into jobs
    Signed-off-by: Arthur De Magalhaes <ademagalhaes@gmail.com>

--- build/buildAll.sh
+++ build/buildAll.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+repository=$1
 tests=(test-pet-clinic test-stock-quote test-stock-trader)
+
+echo "Starting to process repository $repository"
+cd repository
 
 # Builds up the build.sh call to build each individual docker image listed in images.txt
 while read -r buildContextDirectory dockerfile repository imageTag imageTag2 imageTag3
@@ -17,19 +21,16 @@ do
   echo "Running build script - $buildCommand"
   eval $buildCommand
 
-  if [[ $buildContextDirectory =~ latest ]]
-  then
-    # Run through the tests for each of the `latest` tags
-    for test in "${tests[@]}"; do
-      testBuild="./build.sh --dir=$test --dockerfile=Dockerfile --tag=$test --from=$repository:$imageTag"
-      echo "Running build script for test - $testBuild"
-      eval $testBuild
+   #Test the image
+  for test in "${tests[@]}"; do
+    testBuild="./build.sh --dir=$test --dockerfile=Dockerfile --tag=$test --from=$repository:$imageTag"
+    echo "Running build script for test - $testBuild"
+    eval $testBuild
 
-      verifyCommand="./verify.sh $test"
-      echo "Running verify script - $verifyCommand"
-      eval $verifyCommand
-    done
-  fi
+    verifyCommand="./verify.sh $test"
+    echo "Running verify script - $verifyCommand"
+    eval $verifyCommand
+  done
 
   if [ $? != 0 ]; then
     echo "Failed at image $imageTag ($buildContextDirectory) - exiting"
