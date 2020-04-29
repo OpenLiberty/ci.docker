4af5b24d8473341fc67744b068936ea05eabc6ff
Ethan Johnston <ethan.johnston@ibm.com>
2020-04-29 12:08:39 -0400

    Spawn process to keep build alive

--- build/buildx.sh
+++ build/buildx.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+readonly TIMEOUT=1800
+
+function keep_alive() {
+  local i=0
+  while $i -le $TIMEOUT; do
+    local remaining=$(expr $TIMEOUT - $i)
+    echo "*** $remaining seconds remain before timeout..."
+    i=$(expr $i + 120)
+    sleep 120
+  done
+}
+
 cd releases/latest/kernel
 
 echo "Build Context: $(pwd)"
 
+keep_alive &
+
 docker buildx build \
-  --progress auto  \
+  --progress plain  \
   --platform=linux/amd64,linux/ppc64le,linux/s390x \
   -f Dockerfile.ubi.adoptopenjdk11 \
   .
