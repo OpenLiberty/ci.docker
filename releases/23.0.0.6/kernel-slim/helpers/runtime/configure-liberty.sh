791ffde8b3460ec43305c4354a4232fadfa6afec
kabicin <kirbychin99@gmail.com>
2022-08-12 09:28:20 -0700

    Fix folder permissions

--- releases/23.0.0.6/kernel-slim/helpers/runtime/configure-liberty.sh
+++ releases/23.0.0.6/kernel-slim/helpers/runtime/configure-liberty.sh
@@ -4,11 +4,11 @@
 if [ "$SERVER_NAME" != "defaultServer" ] && [ -d "/opt/ol/wlp/usr/servers/defaultServer" ]; then
   # Create new Liberty server
   /opt/ol/wlp/bin/server create --template=javaee8 >/tmp/serverOutput 
-  ret=$?
-  if [ $ret -ne 0 ]; then
+  rc=$?
+  if [ $rc -ne 0 ]; then
     cat /tmp/serverOutput
     rm /tmp/serverOutput
-    exit $ret
+    exit $rc
   fi
   rm /tmp/serverOutput
 
@@ -17,6 +17,7 @@ if [ "$SERVER_NAME" != "defaultServer" ] && [ -d "/opt/ol/wlp/usr/servers/defaul
     echo "The server name contains a character that is not valid."
     exit 1
   fi
+  chmod -R g+w /opt/ol/wlp/usr/servers/$SERVER_NAME
 
   # Delete old symlinks
   rm /opt/ol/links/output
@@ -25,14 +26,23 @@ if [ "$SERVER_NAME" != "defaultServer" ] && [ -d "/opt/ol/wlp/usr/servers/defaul
   # Delete old output folder
   rm -rf /opt/ol/wlp/output/defaultServer
   
-  # Add new output folder symlink
+  # Add new output folder symlink and resolve group write permissions
   mkdir -p $WLP_OUTPUT_DIR/$SERVER_NAME
   ln -s $WLP_OUTPUT_DIR/$SERVER_NAME /opt/ol/links/output
- 
+  chmod g+w $WLP_OUTPUT_DIR/$SERVER_NAME
+  mkdir -p $WLP_OUTPUT_DIR/$SERVER_NAME/resources
+  mkdir -p $WLP_OUTPUT_DIR/$SERVER_NAME/workarea
+  mkdir -p $WLP_OUTPUT_DIR/$SERVER_NAME/logs
+  chmod -R g+w $WLP_OUTPUT_DIR/$SERVER_NAME/workarea
+  chmod -R g+w,o-rwx $WLP_OUTPUT_DIR/$SERVER_NAME/resources
+  chmod -R g+w,o-rwx $WLP_OUTPUT_DIR/$SERVER_NAME/logs
+
   # Add new server symlink and populate folder
   ln -s /opt/ol/wlp/usr/servers/$SERVER_NAME /opt/ol/links/config
   mkdir -p /config/configDropins/defaults
   mkdir -p /config/configDropins/overrides
+  chmod -R g+w /config
+
   cp /opt/ol/wlp/usr/servers/defaultServer/configDropins/defaults/open-default-port.xml /config/configDropins/defaults 
   rm -rf /opt/ol/wlp/usr/servers/defaultServer
 fi
