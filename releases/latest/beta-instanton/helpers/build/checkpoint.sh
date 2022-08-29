ce81891b38d65378d0a88efc2ef2d62742f06ffb
Thomas Watson <tjwatson@us.ibm.com>
2022-08-29 14:51:43 -0500

    Auto configure the checkpoint-1.0 feature if WLP_CHECKPOINT is set
    Instead of forcing the user to enable the checkpoint-1.0 feature
    when doing a checkpoint this will auto configure the feature
    in a config dropin.

--- releases/latest/beta-instanton/helpers/build/checkpoint.sh
+++ releases/latest/beta-instanton/helpers/build/checkpoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# commented out for now to force user to specify checkpoint-1.0 feature
-#Enable checkpoint-1.0 feature
-#echo "<server><featureManager><feature>checkpoint-1.0</feature></featureManager></server>" > /config/configDropins/defaults/checkpoint.xml
-#touch -t 210001010100 /config/configDropins/defaults/checkpoint.xml
-
-# run configure to get the checkpoint-1.0 feature installed
-#echo "Running configure to enable checkpoint-1.0 feature"
-#configure.sh
-
 # hack to bump up the pid by 100
 for i in {1..100}
 do
