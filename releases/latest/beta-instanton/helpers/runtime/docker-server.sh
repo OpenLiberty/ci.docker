1904bcf898d4e1858c06e5b7bcbc54fdef46c296
Thomas Watson <tjwatson@us.ibm.com>
2023-01-23 12:23:36 -0600

    Use restore script to restore

--- releases/latest/beta-instanton/helpers/runtime/docker-server.sh
+++ releases/latest/beta-instanton/helpers/runtime/docker-server.sh
@@ -122,7 +122,7 @@ if [ -d "/output/workarea/checkpoint/image" ]; then
   # A checkpoint image found; exec dumb-init for signal handling.
   # Use of dumb-init for PID 1 is required for signal handling because
   # the restored server process cannot be PID 1.
-  exec dumb-init --rewrite 15:2 -- "$@"
+  exec dumb-init --rewrite 15:2 -- /opt/ol/helpers/runtime/restore-server.sh "$@"
 elif [[ ! -z "$WLP_CHECKPOINT" ]]; then
   # Unset WLP_CHECKPOINT so it is not set in the final image after checkpoint.
   TMP_CHECKPOINT=$WLP_CHECKPOINT
