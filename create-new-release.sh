253f9789996ac30abda64fd91bfa3303199e3364
Wendy Raschke <wraschke@us.ibm.com>
2024-01-30 14:18:59 -0600

    New argument FEATURES_SHA in Dockerfile.ubi.openjdk21 files

--- create-new-release.sh
+++ create-new-release.sh
@@ -47,6 +47,8 @@ for file in $(find ./releases/latest ./releases/$NEW_VERSION -name Dockerfile.*)
    sed -i'.bak' -e "s/ARG LIBERTY_BUILD_LABEL=.*/ARG LIBERTY_BUILD_LABEL=$BUILD_LABEL/g" $file;
 
    sed -i'.bak' -e "s/LIBERTY_SHA=.*/LIBERTY_SHA={replace_with_correct_sha}/" $file;
+   
+   sed -i'.bak' -e "s/FEATURES_SHA=.*/FEATURES_SHA={replace_with_correct_sha}/" $file;
 
    # Clean up temp files
    rm $file.bak
