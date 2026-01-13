f04e26108bb62d644e2962f7b5e4bd4a415585e4
Wendy Raschke <wraschke@us.ibm.com>
2026-01-13 08:47:37 -0800

    Updates for 26.0.0.1

---
+++ releases/26.0.0.1/kernel-slim/helpers/build/internal/logger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function main() {
+    if [ "$VERBOSE" != "true" ]; then
+        exec >/dev/null
+    else
+        set -x
+    fi
+}
+
+function hideLogs() {
+    exec 3>&1 >/dev/null 4>&2 2>/dev/null
+}
+
+function showLogs() {
+    exec 1>&3 3>&- 2>&4 4>&-
+}
+
+main
